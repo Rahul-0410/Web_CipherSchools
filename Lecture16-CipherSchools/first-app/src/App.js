24d3822e565f46231fe27bd9a0746dcd531092b6
Rahul-0410 <rgs786999@gmail.com>
2024-06-08 20:25:26 +0530

    third

---
+++ Lecture16-CipherSchools/first-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name="CipherSchools"
+  return (
+    // <div>
+    <>
+    <div>
+      <h1 style={
+        {
+          color:"red",
+          textAlign:"center",
+        }
+      }>Hello from {name}!</h1>
+    </div>
+
+    <div>
+      Hello from {name}
+    </div>
+    {/* </div> */}
+    </>
+  );
+}
+
+export default App;
