3cd3b924276101bd0c36a9f9faab4c5f7a85ecf3
Rahul-0410 <rgs786999@gmail.com>
2024-06-08 22:34:00 +0530

    third

---
+++ Lecture17-CipherSchools/first-app/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import ToDOScreen from "./screens/ToDoScreen";
+
+class App extends Component{
+    render (){
+      return  <ToDOScreen/>
+    //   <h1>Welcome to Todo List App!</h1>
+    
+    }
+}
+
+export default App;
---
+++ Lecture17-CipherSchools/first-app/src/screens/ToDoScreen.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+class ToDOScreen extends Component{
+    render(){
+        return <h1 class="ui heading center">To Do List</h1>
+    }
+}
+
+export default ToDOScreen;
