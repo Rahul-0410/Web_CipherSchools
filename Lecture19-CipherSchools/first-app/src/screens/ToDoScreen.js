dea387a0acddfae0716dbbed3f92ac2c1c2bc27d
Rahul-0410 <rgs786999@gmail.com>
2024-06-09 22:22:14 +0530

    fourth

--- Lecture19-CipherSchools/first-app/src/screens/ToDoScreen.js
+++ Lecture19-CipherSchools/first-app/src/screens/ToDoScreen.js
@@ -1,8 +1,29 @@
 import { Component } from "react";
 
 class ToDOScreen extends Component{
+    // taskCount=0;
+    state ={
+        taskCount:0,
+    };
     render(){
-        return <h1 class="ui heading center">To Do List</h1>
+        return(
+            <div className="screen">
+                <h1 className="ui heading center">To Do List</h1>
+
+            <div>
+                <button onClick={(e)=>{
+                    // console.log("Add task button was clicked.");
+                    // this.taskCount++;
+                    this.setState({...this.state,
+                        taskCount: this.state.taskCount+1
+                    })
+                    console.log(this.state.taskCount);
+                }} className="ui secondary button">Add Task</button>
+            </div>
+
+            <p>The number of task are: {this.state.taskCount}</p>
+            </div>
+        );
     }
 }
 
